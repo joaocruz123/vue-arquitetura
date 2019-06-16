50da25633baf84856c4b799043e7d67571307aab
Joao <joaofranco.cruz@gmail.com>
2019-06-16 18:48:17 -0300

    Start project

---
+++ src/main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import Vuetify from 'vuetify'
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import colors from 'vuetify/es5/util/colors'
+
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.purple,
+    secondary: colors.grey.darken1,
+    accent: colors.shades.black,
+    error: colors.red.accent3
+  }
+})
+
+//Thema Element-UI
+//import ElementUI from 'element-ui';
+//import 'element-ui/lib/theme-chalk/index.css';
+
+//Vue.use(ElementUI);
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
